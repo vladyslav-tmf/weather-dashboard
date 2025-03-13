a8f6429efc0e6b2f57113a6327af8359e26c54fe
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2025-03-13 11:32:34 +0200

    Add WeatherDataViewSet with filtering

--- weather/views.py
+++ weather/views.py
@@ -4,8 +4,9 @@ from rest_framework.decorators import action
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from weather.models import City
-from weather.serializers import CitySerializer, CityWithCurrentWeatherSerializer
+from weather.models import City, WeatherData
+from weather.serializers import CitySerializer, CityWithCurrentWeatherSerializer, \
+    WeatherDataSerializer
 
 
 class CityViewSet(viewsets.ReadOnlyModelViewSet):
@@ -25,3 +26,30 @@ class CityViewSet(viewsets.ReadOnlyModelViewSet):
         cities = self.get_queryset()
         serializer = CityWithCurrentWeatherSerializer(cities, many=True)
         return Response(serializer.data)
+
+
+class WeatherDataViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint for weather data.
+    Provides read-only access to weather information with filtering.
+    """
+
+    serializer_class = WeatherDataSerializer
+
+    def get_queryset(self) -> QuerySet[WeatherData]:
+        queryset = WeatherData.objects.select_related("city")
+
+        city_id = self.request.query_params.get("city")
+        date_from = self.request.query_params.get("from")
+        date_to = self.request.query_params.get("to")
+
+        if city_id:
+            queryset = queryset.filter(city_id=city_id)
+
+        if date_from:
+            queryset = queryset.filter(timestamp__gte=date_from)
+
+        if date_to:
+            queryset = queryset.filter(timestamp__lte=date_to)
+
+        return queryset
